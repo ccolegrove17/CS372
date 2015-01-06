5cdebf1307a18b0c88de5acbcaf95706ba9b5375
Craig Colegrove <ccolegrove17@my.whitworth.edu>
2015-01-05 16:50:50 -0800

    Homework 1
    Fixed the prime numbers to add modulus

--- Prime Numbers/src/prime/numbers/PrimeNumbers.java
+++ Prime Numbers/src/prime/numbers/PrimeNumbers.java
@@ -19,13 +19,10 @@ public class PrimeNumbers {
         for(int i=1;i<=100;i++)//loop that shows the number being checked (1 to 100)
         {
         boolean isPrime = true; //boolean of if it is prime, starts as yes
-            for(int l=2;l<=i;l++) //loop goes from 2 to the outer number
+            for(int l=2;l<=(i-1);l++) //loop goes from 2 to the outer number
             {
-                for (int m=2;m<=i;m++) //loop goes from 2 to the outer number
-                {
-                    if (l*m==i) //if the product of the inner loops equals the outer number
+                    if (i%l==0) //if there's no remainder
                         isPrime = false; //sets isPrime to false
-                }
             }
             if (isPrime==true) //if isPrime was never set to false
                 System.out.printf("%d\n",(int)i); //displays the number
