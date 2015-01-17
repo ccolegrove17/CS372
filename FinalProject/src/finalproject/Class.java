a977ff377e1775e806aa079a610f8f982ce1aee6
Craig Colegrove <ccolegrove17@my.whitworth.edu>
2015-01-17 14:13:37 -0800

    Final Project Work

--- FinalProject/src/finalproject/Class.java
+++ FinalProject/src/finalproject/Class.java
@@ -6,6 +6,7 @@
 package finalproject;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,25 +16,28 @@ import java.util.regex.Pattern;
  */
 public class Class {
 
+    ArrayList<Class> directory = new ArrayList<Class>();
     String _ID, _name, _faculty;
-    int _credits, _start, _end;
+    int _credits, _start, _end, _length;
     String filename = new String("ClassInfo2.txt");
 
     public Class() {
 
     }
 
-    public Class(String ID, String name, int start, int end, String faculty, int credits) {
+    public Class(String ID, String name, String start, String end, String faculty, int credits) {
+
         _ID = ID;
         _name = name;
         _credits = credits;
-        _start = start;
-        _end = end;
+        _start = convertTime(start);
+        _end = convertTime(end);
         _faculty = faculty;
+        _length = _end - _start;
     }
 
     public void fillClass() {
-        String[] patterns = {"javascript:webAdvisor.newTab\\(\"(.*?) \\(", "\\) (.*?)\",\"", "day (.*?).M - .*?<", " - (..:..).M,.*?<", "</label><input type=\"hidden\" name=\"SEC.FACULTY.INFO_.*?\" value=\"(.*)\">", "</label><input type=\"hidden\" name=\"SEC.MIN.CRED_.*?\" value=\"(.*?)\">"};
+        String[] patterns = {"javascript:webAdvisor.newTab\\(\"(.*?) \\(", "\\) (.*?)\",\"", "day (..:...)M - .*?<", " - (..:...)M,.*?<", "</label><input type=\"hidden\" name=\"SEC.FACULTY.INFO_.*?\" value=\"(.*)\">", "</label><input type=\"hidden\" name=\"SEC.MIN.CRED_.*?\" value=\"(.*?).00\">"};
         try {
             String[] info = {"", "", "", "", "", ""};
             BufferedReader rdr = new BufferedReader(new FileReader(filename));
@@ -44,30 +48,46 @@ public class Class {
                     Pattern p = Pattern.compile(patterns[i]);
                     Matcher m = p.matcher(line);
                     while (m.find()) {
-//                        if (i == 0) {
-//                            info[0] = "";
-//                            info[1] = "";
-//                            info[2] = "";
-//                            info[3] = "";
-//                            info[4] = "";
-//                            info[5] = "";
-//                        }
                         info[i] = m.group(1);
                         if (i == 5) {
-                            System.out.printf(info[0] + " " + info[1] + " " + info[2] + " " + info[3] + " " + info[4] + " " + info[5] + "\n");
+                            Class newClass = new Class(info[0], info[1], info[2], info[3], info[4], Integer.parseInt(info[5]));
+                            //System.out.printf(info[0] + " " + info[1] + " " + info[2] + " " + info[3] + " " + info[4] + " " + info[5] + "\n");
+                            directory.add(newClass);
                         }
-//                        System.out.print(m.group(1) + " ");
-//                        if (i == 5) {
-//                            System.out.println();
-//                        }
                     }
                 }
-                //System.out.printf(info[0] + " " + info[5] + "\n");
             }
             rdr.close();
         } catch (Exception ex) {
             System.out.printf("You fail. %s", ex.getMessage());
         }
+    }
+
+    public int convertTime(String time) {
+        int hours = Integer.parseInt(time.substring(0, 2));
+        int minutes = Integer.parseInt(time.substring(3, 5));
+        String meridiem = time.substring(5, 6);
+        //System.out.printf("%d %d %s", hours, minutes, meridiem);
+        int actualtime = 0;
+        if (hours == 12) {
+            hours = 0;
+        }
+        if (meridiem.equals("P")) {
+            actualtime += 720;
+        }
+        actualtime += hours * 60 + minutes;
+        return actualtime;
+    }
+
+//    public int classLength() {
+//        return 0;
+//    }
+    public void outputInfo(int i) {
+        System.out.println(directory.get(i)._name);
+        System.out.println(directory.get(i)._ID);
+        System.out.println(directory.get(i)._start);
+        System.out.println(directory.get(i)._faculty);
+        System.out.println(directory.get(i)._credits);
     }
 
 }
--- FinalProject/src/finalproject/Interface.java
+++ FinalProject/src/finalproject/Interface.java
@@ -10,7 +10,9 @@ package finalproject;
  * @author Craig
  */
 public class Interface extends javax.swing.JFrame {
+
     Class classes = new Class();
+
     /**
      * Creates new form Interface
      */
@@ -50,12 +52,16 @@ public class Interface extends javax.swing.JFrame {
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         classes.fillClass();
+        classes.outputInfo(288);//error from 288-293
+        //System.out.print(classes.convertTime("04:20P"));
     }//GEN-LAST:event_formWindowOpened
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+
+
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
