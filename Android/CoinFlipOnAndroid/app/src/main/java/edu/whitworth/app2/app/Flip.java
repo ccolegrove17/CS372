2d4d85920434b0f3312a70e8e93be381a73b8bd6
Peter Tucker <ptucker@whitworth.edu>
2015-02-05 19:53:09 -0800

    Added feedback

--- Android/CoinFlipOnAndroid/app/src/main/java/edu/whitworth/app2/app/Flip.java
+++ Android/CoinFlipOnAndroid/app/src/main/java/edu/whitworth/app2/app/Flip.java
@@ -15,6 +15,7 @@ public class Flip {
     public String flipCoin(){
         int flip = rand.nextInt(2);//random number 0 or 1
         if (flip==0)//if zero
+        //PT -- this is fine, but maybe better to return a 0/1, and let the UI decide how to display it (e.g. text, image, ...)
             return "Heads!";//heads
         else//otherwise
             return "Tails!";//tails
